cffe58ec218a997b545f57405cccee92fdd9599f
DevelopeJr <edemir.ferreira.jr@gmail.com>
2022-02-14 10:15:11 -0400

    Add project files.

---
+++ src/DevT800.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevT800.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        /*public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "desenvolvedor.io";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+        http://www.macoratti.net/17/12/aspncore_ctaghlp1.htm
+        
+        Obs: Versao acima não funciona
+        
+         */
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string emailTo = context.AllAttributes["mailTo"].Value.ToString();
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + emailTo);
+            output.Content.SetContent("developer.backend@icloud.com");
+        }
+    }
+}
